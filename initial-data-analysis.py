7232cd88b02fbddc41bafff46e4a2aef36a31fb3
Ben Caterine <benrcaterine@gmail.com>
2022-02-09 16:41:00 -0600

    analysis for proposal

--- initial-data-analysis.py
+++ initial-data-analysis.py
@@ -19,31 +19,3 @@ print(df)
 for stat in stats_cols.keys():
     print(stat, 'mean:', df[stats_cols[stat]].mean())
     print(stat, 'std:', df[stats_cols[stat]].std())
-
-# df = pd.read_csv(stats)
-# print('points mean:', df['pts'].mean())
-# print('points std:', df['pts'].std())
-
-# #assists
-# df = pd.read_csv(stats)
-# print('points mean:', df['ast'].mean())
-# print('points std:', df['ast'].std())
-
-# #total rebounds
-# df = pd.read_csv(stats)
-# print('points mean:', df['treb'].mean())
-# print('points std:', df['treb'].std())
-
-# #blocks
-# df = pd.read_csv(stats)
-# print('points mean:', df['blk_per'].mean())
-# print('points std:', df['blk_per'].std())
-
-# #true shooting %
-# df = pd.read_csv(stats)
-# print('points mean:', df['TS_per'].mean())
-# print('points std:', df['TS_per'].std())
-
-# #recruiting rank
-# print('points mean:', df['Rec rank'].mean())
-# print('points std:', df['Rec rank'].std())
