4dc280acb4df32d93da0f3800566195f43459c87
Ben Caterine <benrcaterine@gmail.com>
2022-02-26 17:21:20 -0600

    added confounders

--- initial-data-analysis.py
+++ initial-data-analysis.py
@@ -59,18 +59,26 @@ df.rename(
         'LANE_AGILITY_TIME_(SECONDS)': 'lane',
         'THREE_QUARTER_SPRINT_(SECONDS)': 'sprint',
         'STANDING_VERTICAL_LEAP_(INCHES)': 'vert',
-        'MAX_VERTICAL_LEAP_(INCHES)': 'mvert'
+        'MAX_VERTICAL_LEAP_(INCHES)': 'mvert',
+        'ast/tov': 'atr'
     },
     axis=1,
     inplace=True
 )
 df['drafted'] = ~df['pick'].isnull()*1
-df['lane'] = pd.cut(df['lane'], 10)
-df['sprint'] = pd.cut(df['sprint'], 10)
-df['vert'] = pd.cut(df['vert'], 10)
-df['mvert'] = pd.cut(df['mvert'], 10)
-print(df['vert'])
-specific_df = df[['lane', 'sprint', 'vert', 'mvert', 'drafted', 'hand_length', 'hand_width', 'height', 'reach', 'weight', 'pts', 'treb', 'ast']]
+df['lane'] = pd.qcut(df['lane'], 10)
+df['sprint'] = pd.qcut(df['sprint'], 10)
+df['vert'] = pd.qcut(df['vert'], 10)
+df['mvert'] = pd.qcut(df['mvert'], 10)
+combine_stats_to_use = [
+    'fat', 'hand_length', 'hand_width', 'height', 'reach', 'weight', 'WINGSPAN'
+]
+college_stats_to_use = [
+    'Min_per', 'Ortg', 'usg', 'eFG', 'TS_per', 'TO_per', 'FT_per', 'twoP_per', 'TP_per', 'ftr',
+    'porpag', 'adjoe', 'atr', 'drtg', 'adrtg', 'dporpag', 'stops', 'bpm', 'obpm', 'dbpm',
+    'gbpm', 'oreb', 'dreb', 'treb', 'ast', 'stl', 'blk', 'pts'
+]
+specific_df = df[['drafted', 'lane', 'vert'] + combine_stats_to_use + college_stats_to_use]
 specific_df.dropna(inplace=True)
 print(specific_df)
 
@@ -78,15 +86,12 @@ est = estimator.bootstrap(
     specific_df,
     function=estimator.backdoor,
     n=100,
-    intervention=['vert', 'mvert'],
+    intervention=['lane', 'vert'],
     outcome='drafted',
-    confounders=['hand_length', 'hand_width', 'height', 'reach', 'weight', 'pts', 'treb', 'ast']
+    confounders = combine_stats_to_use + college_stats_to_use
 )
 print(est)
 
 # TODO:
 # - interpret CI matrix
-# - discretize/invert combine data to fit the model
-# - decide which C and M values to use
-# - figure out what n is feasible based on number of C/Ms
 # - nonbinary drafted outcome?
